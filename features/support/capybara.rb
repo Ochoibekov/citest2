cd5f812fa4b92fb5f6390989871d451941725ba3
Omurbek Choibekov <omka3s@gmail.com>
2017-12-25 11:57:06 +0600

    testing2131

--- features/support/capybara.rb
+++ features/support/capybara.rb
@@ -4,26 +4,20 @@ require 'capybara/dsl'
 require 'selenium-cucumber'
 require 'selenium/webdriver'
 
-# Selenium::WebDriver::Chrome.driver_path = "./chromedriver"
+Selenium::WebDriver::Chrome.driver_path = "./chromedriver"
 
-# Capybara.register_driver :chrome do |app|
-# 	chrome_options = {
-#     browser: :chrome,
-#     options: ::Selenium::WebDriver::Chrome::Options.new()
-#   }
-#   chrome_options[:options].args << 'headless'
-#   Capybara::Selenium::Driver.new(app, chrome_options)
-# end
+Capybara.register_driver :chrome do |app|
+	chrome_options = {
+    browser: :chrome,
+    options: ::Selenium::WebDriver::Chrome::Options.new()
+  }
+  # chrome_options[:options].args << 'headless'
+  Capybara::Selenium::Driver.new(app, chrome_options)
+end
 
-# Capybara.javascript_driver = :chrome
+Capybara.javascript_driver = :chrome
 
-# Capybara.default_driver = Capybara.javascript_driver
-# Capybara.app_host = 'http://localhost:9292/'
-
-driver = :selenium
-
-Capybara.default_driver = driver
-# Capybara.app_host = 'http://cp.kwikichat.com/'
+Capybara.default_driver = Capybara.javascript_driver
 Capybara.app_host = 'http://localhost:9292/'
-Capybara.page.driver.browser.manage.window.resize_to(1440, 900)
+
 
